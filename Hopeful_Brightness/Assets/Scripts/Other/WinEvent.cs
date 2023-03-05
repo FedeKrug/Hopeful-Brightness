2d4226ff4103edc9bb90226a5ca41ebce1a0bc3f
FedeKrug <86620474+FedeKrug@users.noreply.github.com>
2023-03-05 20:55:23 -0300

    TextMeshPro

--- Hopeful_Brightness/Assets/Scripts/Other/WinEvent.cs
+++ Hopeful_Brightness/Assets/Scripts/Other/WinEvent.cs
@@ -9,11 +9,15 @@ using UnityEngine.Playables;
 public class WinEvent : MonoBehaviour
 {
 	[SerializeField] PlayableDirector _director;
+	[SerializeField] PlayerMovement _playerRef;
+	[SerializeField] PlayerJump _playerJumpRef;
 	private void OnTriggerEnter2D(Collider2D collision)
 	{
 		if (collision.CompareTag("Player"))
 		{
 			_director.Play();
+			_playerRef.enabled = false;
+			_playerJumpRef.enabled = false;
 		}
 	}
 }
